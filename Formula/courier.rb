1ee79e2aea82af9d65a4980de9ea77f44b420520
blutui-ci <info@blutui.com>
2020-08-26 12:05:05 +1200

    Brew formula update for courier version v0.2.4

--- Formula/courier.rb
+++ Formula/courier.rb
@@ -2,16 +2,16 @@
 class Courier < Formula
   desc "Blutui Courier CLI"
   homepage "https://blutui.com"
-  version "0.2.3"
+  version "0.2.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://cdn.blutui.com/courier/v0.2.3/courier_0.2.3_macOS_64-bit.tar.gz"
-    sha256 "7638538dc9f14757bd57f901d8172080df1ebefdb00aaf88a8c97be2817d9c5d"
+    url "https://cdn.blutui.com/courier/v0.2.4/courier_0.2.4_macOS_64-bit.tar.gz"
+    sha256 "de80efc7cd183a6fba8d7f1859dbd83f6ef4854b5a5f354cd0544e3ccf48654c"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://cdn.blutui.com/courier/v0.2.3/courier_0.2.3_linux_64-bit.tar.gz"
-      sha256 "47ede62fe9ab271e4d94f0850eeda304e0a1c8cf9304ea68d32b271a85c237a8"
+      url "https://cdn.blutui.com/courier/v0.2.4/courier_0.2.4_linux_64-bit.tar.gz"
+      sha256 "35a9b1bcffdff99d18a08bb69cad016d95376ac71f648583ac1fe7b31c4029f2"
     end
   end
 
