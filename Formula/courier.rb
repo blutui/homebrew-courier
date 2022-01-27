f25e16c1f63bf174c9b6a43b4f8e3cb0f7326b36
blutui-ci <contact@blutui.com>
2022-01-27 14:37:28 +1300

    Brew formula update for courier version v0.2.13

--- Formula/courier.rb
+++ Formula/courier.rb
@@ -5,25 +5,28 @@
 class Courier < Formula
   desc "Blutui Courier CLI"
   homepage "https://blutui.com"
-  version "0.2.12"
-  bottle :unneeded
+  version "0.2.13"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://cdn.blutui.com/courier/v0.2.12/courier_0.2.12_macOS_64-bit.tar.gz"
-      sha256 "02c1e10dd9b5c4fe7d7c1c94402545d0174184867fa8582081349d2872fa72b5"
+      url "https://cdn.blutui.com/courier/v0.2.13/courier_0.2.13_macOS_64-bit.tar.gz"
+      sha256 "3a1d6295a58debdcf5c8a4e58f7f3357c7cffcf2d9d341dceab7207f59c06c58"
+
+      def install
+        bin.install "courier"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://cdn.blutui.com/courier/v0.2.12/courier_0.2.12_linux_64-bit.tar.gz"
-      sha256 "42244da89150bd9906e661def3da41d45964102b7bdf5909a81ec7be668fc151"
-    end
-  end
+      url "https://cdn.blutui.com/courier/v0.2.13/courier_0.2.13_linux_64-bit.tar.gz"
+      sha256 "11cd7671be1ffa77641b061d047ee231060124ba831f3d6a279e65eb3a0971eb"
 
-  def install
-    bin.install "courier"
+      def install
+        bin.install "courier"
+      end
+    end
   end
 
   def caveats; <<~EOS
